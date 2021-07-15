a0d2ebbb0de4457e6a4caf9b090dbbd9444d5bf2
unmyr <56982541+unmyr@users.noreply.github.com>
2021-07-16 01:11:33 +0900

    test(unit): Add a test to press the minus button.

--- tests/unit/comp-Counter.spec.js
+++ tests/unit/comp-Counter.spec.js
@@ -2,11 +2,23 @@ import { shallowMount } from '@vue/test-utils'
 import Counter from '@/components/Counter.vue'
 
 describe('Counter.vue', () => {
-  it('renders count-view when passed', () => {
+  it('click plus button', async () => {
     const step = 1
     const wrapper = shallowMount(Counter, {
       propsData: { step }
     })
     expect(wrapper.vm.count).toBe(0)
+    await wrapper.find("button[name='plus']").trigger('click')
+    expect(wrapper.vm.count).toBe(1)
+  })
+
+  it('click minus button', async () => {
+    const step = 1
+    const wrapper = shallowMount(Counter, {
+      propsData: { step }
+    })
+    expect(wrapper.vm.count).toBe(0)
+    await wrapper.find("button[name='minus']").trigger('click')
+    expect(wrapper.vm.count).toBe(-1)
   })
 })
