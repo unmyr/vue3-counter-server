03b5b7ffe6c3588903d1d5d08c870425479ace84
unmyr <56982541+unmyr@users.noreply.github.com>
2021-08-09 23:10:00 +0900

    style: Fix Prettier warnings.

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,12 +1,11 @@
+import { Commit } from "vuex";
+
 export default {
-  increment ({ commit }: any, param: number) {
-    commit('setLoading', true)
-    setTimeout(
-      () => {
-        commit('increment', param)
-        commit('setLoading', false)
-      },
-      500
-    )
-  }
-}
+  increment({ commit }: { commit: Commit }, param: number): void {
+    commit("setLoading", true);
+    setTimeout(() => {
+      commit("increment", param);
+      commit("setLoading", false);
+    }, 500);
+  },
+};
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,17 @@
-import { InjectionKey } from 'vue'
-import { createStore, Store } from 'vuex'
-import { IState } from '@/store/IState'
-import mutations from '@/store/mutations'
-import actions from '@/store/actions'
+import { InjectionKey } from "vue";
+import { createStore, Store } from "vuex";
+import { IState } from "@/store/IState";
+import mutations from "@/store/mutations";
+import actions from "@/store/actions";
 
 export const store = createStore<IState>({
   state: {
     count: 0,
-    isLoading: false
+    isLoading: false,
   },
   mutations,
   actions,
-  modules: {
-  }
-})
+  modules: {},
+});
 
-export const key: InjectionKey<Store<IState>> = Symbol('global store')
+export const key: InjectionKey<Store<IState>> = Symbol("global store");
