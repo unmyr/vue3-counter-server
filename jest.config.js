1679e74a3fffcd430e648d59aba72fc9e6573f87
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-10 12:32:10 +0900

    test: Replace vue-jest@5.0.0-0 to "@vue/vue3-jest@27.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript",
   transform: {
-    "^.+\\.vue$": "vue-jest",
+    "^.+\\.vue$": "@vue/vue3-jest",
     "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
   },
   collectCoverage: true,
