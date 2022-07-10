754da1a4c8d6ff8a599176295a3c5de523511bc0
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-10 20:03:45 +0900

    test: Update chrome driver and change path for production.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/vue3-counter-server/" : "/",
-};
+const { defineConfig } = require("@vue/cli-service");
 
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath:
+    process.env.NODE_ENV === "gh-pages" ? "/vue3-counter-server/" : "/",
+});
