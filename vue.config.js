87a22df1c9275f829873d34771810cba80027444
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-10 14:36:20 +0900

    ci: Change deploy mode for e2e tests.

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath:
-    process.env.NODE_ENV === "production" ? "/vue3-counter-server/" : "/",
+    process.env.NODE_ENV === "gh-pages" ? "/vue3-counter-server/" : "/",
 });
